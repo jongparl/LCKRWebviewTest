7191471390f2cd5c8f809bea38f97e45af4f0778
j.park <j.park@criteo.com>
2020-09-24 10:53:39 +0900

    first commit

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    //private WebView webView;
+    private String url = "https://www.daum.net";
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.webtest1);
+
+        mWebView.getSettings().setJavaScriptEnabled(true);
+
+        mWebView.loadUrl(url);
+        mWebView.setWebChromeClient(new WebChromeClient());
+        mWebView.setWebViewClient(new HelloWebViewClient());
+
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+            mWebView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+
+    }
+
+    private class HelloWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+}
